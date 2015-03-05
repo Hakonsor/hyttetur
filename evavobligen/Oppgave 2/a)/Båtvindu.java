f3de28cd187390cc3b79525411e39fb1ad799bc9
Barnholt <Magnus@dhcp-158-39-194-67.wlan.hioa.no>
2015-03-05 11:23:53 +0100

    Slettet fil
    Slette gammel båtvindu fil

--- evavobligen/Oppgave 2/a)/Båtvindu.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Båtvindu{
-	private JTextField forfatterfelt, tittelfelt, sideantallsfelt, prisfelt, fagfelt, skolefagfelt, klassetrinnfelt, sjangerfelt, målformfelt, språkfelt;
-	private JButton regFagbok, regSkolebok, regNRoman, regURoman, visReg;
-	private JTextArea utskriftsområde;
-	private Bokregister register;
-	private String filnavn = "bokdata.dta";
-}
